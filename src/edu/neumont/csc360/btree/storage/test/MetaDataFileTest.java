e44aac7c1be6c2ebbac95d70499803245f518107
Tyler Berry <tyler@berryac.com>
2016-05-17 12:46:06 -0600

    extracted variable for testfile

--- src/edu/neumont/csc360/btree/storage/test/MetaDataFileTest.java
+++ src/edu/neumont/csc360/btree/storage/test/MetaDataFileTest.java
@@ -11,34 +11,36 @@ import java.nio.file.Paths;
 import static org.junit.Assert.*;
 
 public class MetaDataFileTest {
+    public static final String testFile = "testfile";
+    
     @Test
     public void getMetaDataSize() throws Exception {
-        Files.deleteIfExists(Paths.get("testfile"));
-        MetaDataFile.create("testfile", 24);
-        MetaDataFile metaDataFile = MetaDataFile.open("testfile");
+        Files.deleteIfExists(Paths.get(testFile));
+        MetaDataFile.create(testFile, 24);
+        MetaDataFile metaDataFile = MetaDataFile.open(testFile);
         Assert.assertEquals(24, metaDataFile.getMetaDataSize());
     }
 
     @Test
     public void metaDataReadWrite() throws Exception {
-        Files.deleteIfExists(Paths.get("testfile"));
-        MetaDataFile.create("testfile", 4);
-        MetaDataFile metaDataFile = MetaDataFile.open("testfile");
+        Files.deleteIfExists(Paths.get(testFile));
+        MetaDataFile.create(testFile, 4);
+        MetaDataFile metaDataFile = MetaDataFile.open(testFile);
 
         int[] metaData = new int[] { 0, 1, 2, 3 };
         metaDataFile.writeMetaData(metaData);
         Assert.assertArrayEquals(metaData, metaDataFile.getMetaData());
 
         metaDataFile.close();
-        metaDataFile = MetaDataFile.open("testfile");
+        metaDataFile = MetaDataFile.open(testFile);
         Assert.assertArrayEquals(metaData, metaDataFile.getMetaData());
     }
 
     @Test
     public void readWrite() throws Exception {
-        Files.deleteIfExists(Paths.get("testfile"));
-        MetaDataFile.create("testfile", 256);
-        MetaDataFile metaDataFile = MetaDataFile.open("testfile");
+        Files.deleteIfExists(Paths.get(testFile));
+        MetaDataFile.create(testFile, 256);
+        MetaDataFile metaDataFile = MetaDataFile.open(testFile);
 
         int[] buffer = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
         metaDataFile.write(buffer, 0);
