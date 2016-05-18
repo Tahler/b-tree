6bdf1ae35c259b51494154d8b04f5e3e5c9bb27e
Tyler Berry <tyler@berryac.com>
2016-05-18 11:34:50 -0600

    easily implemented CachedPersistentArray with copied cache

---
+++ src/edu/neumont/csc360/btree/storage/CachedPersistentArray.java
@@ -0,0 +1,69 @@
+package edu.neumont.csc360.btree.storage;
+
+import edu.neumont.csc360.btree.storage.cache.Cache;
+
+public class CachedPersistentArray {
+    private static final int CACHE_SIZE = 10;
+
+    private PersistentArray persistentArray;
+    private Cache<Integer, Buffer> cache;
+
+    private CachedPersistentArray() {}
+
+    public static void create(String name, int metadataSize, int bufferSize, int cacheSize) {
+        PersistentArray.create(name, metadataSize, bufferSize);
+    }
+
+    public static CachedPersistentArray open(String name) {
+        CachedPersistentArray cachedPersistentArray = new CachedPersistentArray();
+        cachedPersistentArray.persistentArray = PersistentArray.open(name);
+        cachedPersistentArray.cache = new Cache<>(CACHE_SIZE);
+        return cachedPersistentArray;
+    }
+
+    public static void delete(String name) {
+        PersistentArray.delete(name);
+    }
+
+    public void close() {
+        this.persistentArray.close();
+    }
+
+    public int[] getMetadata() {
+        return this.persistentArray.getMetadata();
+    }
+
+    public void writeMetadata(int[] metadata) {
+        this.persistentArray.writeMetadata(metadata);
+    }
+
+    public int allocate() {
+        return this.persistentArray.allocate();
+    }
+
+    public void deallocate(int index) {
+        if (this.cache.containsKey(index)) {
+            this.cache.remove(index);
+        }
+        this.persistentArray.deallocate(index);
+    }
+
+    public int[] getBuffer(int index) {
+        return this.cache.containsKey(index)
+                ? this.cache.get(index).data
+                : this.persistentArray.getBuffer(index);
+    }
+
+    public void putBuffer(int index, int[] buffer) {
+        this.cache.put(index, new Buffer(buffer));
+        this.persistentArray.putBuffer(index, buffer);
+    }
+
+    private class Buffer {
+        int[] data;
+
+        public Buffer(int[] data) {
+            this.data = data;
+        }
+    }
+}
