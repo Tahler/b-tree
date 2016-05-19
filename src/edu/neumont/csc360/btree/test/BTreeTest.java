797a44db37aed7d70329d47403cb979674951d16
Tyler Berry <tyler@berryac.com>
2016-05-19 11:18:34 -0600

    added a test file for BTree

---
+++ src/edu/neumont/csc360/btree/test/BTreeTest.java
@@ -0,0 +1,28 @@
+package edu.neumont.csc360.btree.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BTreeTest {
+    @Test
+    public void addKey() throws Exception {
+
+    }
+
+    @Test
+    public void getValue() throws Exception {
+
+    }
+
+    @Test
+    public void updateKeysValue() throws Exception {
+
+    }
+
+    @Test
+    public void deleteKey() throws Exception {
+
+    }
+
+}
